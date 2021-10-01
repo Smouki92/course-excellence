333488c478af0a8226fcb6dcea2bc448658df7be
Alehander <serheev24@gmail.com>
2021-10-01 10:27:10 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="Conteiner">
-      <header className="header">
-        <h1>Hello World</h1>
-      </header>
+    <>
+    <header className="header">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">BrandName</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#product">Product</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+            <Nav>
+            <Button variant="light" size="lg" className="me-4">Login</Button>
+            <Button variant="primary" size="lg" >JOIN US</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+    
+    <div >
+
     </div>
+    </>
   );
 }
 
